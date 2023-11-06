89b7131a911b572c7369d61da0129f6fd6b15159
aloks <sarkarsworld@gmail.com>
2023-11-07 00:05:06 +0530

    Added AntMatchers and targeting specific URI's with Roles.

--- src/main/java/com/simplesolutions/config/ApplicationSecurityConfig.java
+++ src/main/java/com/simplesolutions/config/ApplicationSecurityConfig.java
@@ -15,6 +15,11 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter  {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/public/**").hasRole("NORMAL")    // URI's starting with /public can be accessed by NORMAL users only.
+                .antMatchers("/users/**").hasRole("ADMIN")   // URI's starting with /users can be accessed by ADMIN users only.
+
+//                .antMatchers("/public/**").permitAll()      // for permitting a unified or generic URI patter.
+//                .antMatchers("/home", "/login", "register").permitAll()  // for matching specific URI's
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -24,9 +29,9 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter  {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .inMemoryAuthentication().withUser("Admin").password(this.passwordEncoder().encode("Admin")).roles("Admin");
+                .inMemoryAuthentication().withUser("Admin").password(this.passwordEncoder().encode("Admin")).roles("ADMIN");
         auth
-                .inMemoryAuthentication().withUser("Normal").password(this.passwordEncoder().encode("Normal")).roles("Normal");
+                .inMemoryAuthentication().withUser("Normal").password(this.passwordEncoder().encode("Normal")).roles("NORMAL");
     }
 
 
