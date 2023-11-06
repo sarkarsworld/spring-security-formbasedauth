b6f02d5032da1446a5f5fad5de13f32377638e89
aloks <sarkarsworld@gmail.com>
2023-11-06 19:30:58 +0530

    Initial commit

---
+++ src/main/java/com/simplesolutions/SpringSecurityFormBasedAuthApplication.java
@@ -0,0 +1,13 @@
+package com.simplesolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityFormBasedAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityFormBasedAuthApplication.class, args);
+	}
+
+}
