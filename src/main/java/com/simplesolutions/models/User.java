4736c4bdf750b6d99621b29db3977ff3f6d84211
aloks <sarkarsworld@gmail.com>
2023-11-07 11:10:19 +0530

    DB connection created with Spring Data JPA

--- src/main/java/com/simplesolutions/models/User.java
+++ src/main/java/com/simplesolutions/models/User.java
@@ -5,12 +5,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class User {
 
+    @Id
     private String userName;
     private String password;
     private String email;
---
+++ src/main/java/com/simplesolutions/repo/UserRepo.java
@@ -0,0 +1,9 @@
+package com.simplesolutions.repo;
+
+import com.simplesolutions.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, String> {
+}
